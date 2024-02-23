6a278f76111c17039d4933bc85058e515b73444f
Marie <ma-aldebert@hotmail.fr>
2024-02-23 10:23:29 +0000

    weather api call

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
+function refreshWeather(response) {
+  console.log(response.data);
+}
+
+function searchCity(city) {
+  let apiKey = "ef7417d18a5fa20et4ba9o259132407e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
 
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
