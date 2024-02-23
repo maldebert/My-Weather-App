f6a781aed1eef3a4d618b0cc95bb029c9625a3bc
Marie <ma-aldebert@hotmail.fr>
2024-02-23 21:50:14 +0000

    js forecase update

--- index.js
+++ index.js
@@ -56,7 +56,29 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+<div class="weather-forecast-day">
+<div class="weather-forecast-date">${day}</div>
+<div class="weather-forecast-icon">🌧️</div>
+<div class="weather-forecast-temperature">
+<span class="weather-forecast-temperature-max">18° </span><span class="weather-forecast-temperature-min">12°</span>
+</div>
+</div>`;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innherHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Edinburgh");
+displayForecast();
